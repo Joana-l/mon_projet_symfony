df371ef75a9d18e3d9abd92518b99a6e31ab23df
laffitte joana <ljoanaPB@gmail.com>
2025-05-19 14:42:34 +0100

    test: démarrage manuel de la session sur /login

--- src/Controller/YController.php
+++ src/Controller/YController.php
@@ -6,12 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class YController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, RequestStack $requestStack): Response
     {
+        $session = $requestStack->getSession();
+        $session->set('force_csrf_debug', true);
         
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
